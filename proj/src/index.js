c4105515fb51494efba2dab491623c470c436e4d
Jacek Góraj <jckgrj@gmail.com>
2018-06-06 15:57:47 +0200

    WIP

--- proj/src/index.js
+++ proj/src/index.js
@@ -1,4 +1,5 @@
 import _ from 'lodash';
+let h337 = require('heatmap.js');
 
 function component() {
     var element = document.createElement('div');
@@ -6,4 +7,34 @@ function component() {
     return element;
 }
 
+function createHeatmap() {
+var heatmapInstance = h337.create({
+    container: document.querySelector('.heatmap')
+  });
+  
+  var points = [];
+  var max = 0;
+  var width = 840;
+  var height = 400;
+  var len = 200;
+  
+  while (len--) {
+    var val = Math.floor(Math.random()*100);
+    max = Math.max(max, val);
+    var point = {
+      x: Math.floor(Math.random()*width),
+      y: Math.floor(Math.random()*height),
+      value: val
+    };
+    points.push(point);
+  }
+
+  var data = { 
+    max: max, 
+    data: points 
+  };
+
+  heatmapInstance.setData(data);
+}
+createHeatmap();
 document.body.appendChild(component());
